8ad8f967e62fd633f60dae2d40f9d86a0fbd15ab
roa_0318 <roa_0318@naver.com>
2024-03-10 16:11:57 +0900

    24.03.10

--- practice.py
+++ practice.py
@@ -4,52 +4,49 @@ N, M = map(int, input().split())
 
 rooms = [[0] * N for _ in range(N)]
 rooms[0][0] = 1
-
 visited = [[0] * N for _ in range(N)]
 visited[0][0] = 1
 
 switch = []
 
-ans = 0
-
 for _ in range(M):
   x, y, a, b = map(int, input().split())
   switch.append([x, y, a, b])
 
-queue = deque()
-queue.append((0, 0))
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
 
-da = [-1, 1, 0, 0]
-db = [0, 0, -1, 1]
+def bfs(x, y):
+  ans = 1
+  queue = deque()
+  queue.append((x, y))
 
-while queue:
-  tmp = []
-  r, c = queue.popleft()
+  while queue:
+    r, c = queue.popleft()
 
-  for x, y, a, b in switch:
-    if r == x - 1 and c == y - 1:
-      rooms[a - 1][b - 1] = 1
+    for x, y, a, b in switch:
+      if r == x - 1 and c == y - 1:
+        if not rooms[a - 1][b - 1]:
+          rooms[a - 1][b - 1] = 1
+          ans += 1
         
-      for i in range(4):
-        na = (a - 1) + da[i]
-        nb = (b - 1) + db[i]
+          for i in range(4):
+            nr = (a - 1) + dr[i]
+            nc = (b - 1) + dc[i]
 
-        if 0 <= na < N and 0 <= nb < N:
-          if visited[na][nb] == 1:
-            queue.appendleft((a - 1, b - 1))
-            visited[a - 1][b - 1] = 1
+            if 0 <= nr < N and 0 <= nc < N:
+              if visited[nr][nc] == 1:
+                queue.append((nr, nc))
+      
+    for j in range(4):
+      nr = r + dr[j]
+      nc = c + dc[j]
 
-          if visited[na][b] == 0 and rooms[na][nb] == 1:
-            tmp.append((na, nb))
+      if 0 <= nr < N and 0 <= nc < N:
+        if rooms[nr][nc] == 1 and not visited[nr][nc]:
+          queue.append((nr, nc))
+          visited[nr][nc] = 1
 
-  queue = tmp[:]
+  return ans
 
-
-
-
-for room in rooms:
-  for right in room:
-    if right == 1:
-      ans += 1
-
-print(ans)
+print(bfs(0, 0))
