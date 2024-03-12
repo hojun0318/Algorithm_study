95f05d2fec7a91455e267a92750c4a410f7a229f
roa_0318 <roa_0318@naver.com>
2024-03-12 22:05:30 +0900

    24.03.12

--- practice.py
+++ practice.py
@@ -1,56 +1,30 @@
-from collections import deque, defaultdict
+import sys
+sys.setrecursionlimit(111111) #충분한 재귀 깊이를 주어 오류를 예방
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
-T = int(input())
+def dfs(x):
+    global result
+    visited[x] = True
+    cycle.append(x) #사이클을 이루는 팀을 확인하기 위함
+    number = numbers[x]
+    
+    if visited[number]: #방문가능한 곳이 끝났는지
+        if number in cycle: #사이클 가능 여부
+            result += cycle[cycle.index(number):] #사이클 되는 구간 부터만 팀을 이룸
+        return
+    else:
+        dfs(number)
 
-def bfs():
-  global ans, visited
-  queue = deque()
-  queue.append((0, 0))
-  visited[0][0] = 1
 
-  while queue:
-    x, y = queue.popleft()
-
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-
-      if 0 <= nx < h + 2 and 0 <= ny < w + 2 and not visited[nx][ny]:
-        if buildings[nx][ny] == '*':
-          continue
-
-        if 'A' <= buildings[nx][ny] <= 'Z':
-          if buildings[nx][ny].lower() not in keys:
-            continue
+for _ in range(int(input())):
+    N = int(input())
+    numbers = [0] + list(map(int, input().split()))
+    visited = [True] + [False] * N #방문 여부
+    result = []
+    
+    for i in range(1, N+1):
+        if not visited[i]: #방문 안한 곳이라면
+            cycle = []
+            dfs(i) #DFS 함수 돌림
             
-        elif 'a' <= buildings[nx][ny] <= 'z':
-            if buildings[nx][ny] not in keys:
-              keys[buildings[nx][ny]] = buildings[nx][ny].upper()
-              visited = [[0] * (w + 2) for _ in range(h + 2)]
-
-        elif buildings[nx][ny] == '$' and (nx, ny) not in visited_document:
-          ans += 1
-          visited_document.append((nx, ny))
-
-        visited[nx][ny] = 1
-        queue.append((nx, ny))
-
-  return ans
-
-for _ in range(1, T + 1):
-  h, w = map(int, input().split())
-  buildings = [['.'] + list(map(str, input())) + ['.'] for _ in range(h)]
-  buildings = [['.'] * (w + 2)] + buildings + [['.'] * (w + 2)]
-  visited = [[0] * (w + 2) for _ in range(h + 2)]
-  keys = defaultdict(int)
-  visited_document = []
-  ans = 0
-
-  for key in input():
-    if key.isalpha():
-      keys[key] = key.upper()
-
-  print(bfs())
+    print(N - len(result)) #팀에 없는 사람 수
