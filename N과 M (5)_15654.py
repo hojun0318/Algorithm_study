e562d62ae4414a498a0a403524789b3c1da6e6c1
roa_0318 <roa_0318@naver.com>
2024-03-22 17:47:28 +0900

    24.03.22

---
+++ N과 M (5)_15654.py
@@ -0,0 +1,22 @@
+def dfs(n, lst):
+  if n == M:
+    ans.append(lst)
+    return
+  
+  for i in range(N):
+    if visited[i] == 0:
+      visited[i] = 1
+      dfs(n + 1, lst + [arr[i]])
+      visited[i] = 0
+
+N, M = map(int, input().split())
+arr =list(map(int, input().split()))
+arr.sort()
+
+visited = [0] * (N + 1)
+ans = []
+
+dfs(0, [])
+
+for lst in ans:
+  print(*lst)
