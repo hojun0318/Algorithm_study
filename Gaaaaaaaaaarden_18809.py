bb2558fc2401d9b7b11b91aba44a9a334e970cb9
roa_0318 <roa_0318@naver.com>
2024-03-28 08:39:28 +0900

    24.03.28

--- Gaaaaaaaaaarden_18809.py
+++ Gaaaaaaaaaarden_18809.py
@@ -1,63 +1,71 @@
 from collections import deque
 
 def bfs(tlst):
-  global cnt
   queue = deque()
-  v = [[0] * (M + 2) for _ in range(N + 2)]
+  visited = [[0] * (M + 2) for _ in range(N + 2)]
+  cnt = 0
 
   for i in range(TC):
     if tlst[i] == 0:
       continue
+
     ti, tj = lst[i]
     queue.append((ti, tj))
-    v[ti][tj] = tlst[i]
+    visited[ti][tj] = tlst[i]
 
-    while queue:
-      ci, cj = queue.popleft()
-      
-      if v[ci][cj] == 25000:
+  while queue:
+    ci, cj = queue.popleft()
+    if visited[ci][cj] == 25000:
+      continue
+
+    for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+      ni = ci + di
+      nj = cj + dj
+
+      if arr[ni][nj] == 0 or visited[ni][nj] == 25000:
         continue
 
-      for di, dj in ((1, 0), (-1, 0), (0, -1), (0, 1)):
-        ni, nj = ci + di, cj + dj
-
-        if arr[ni][nj] == 0 or v[ni][nj] == 25000:
-          continue
-
-        if v[ni][nj] == 0:
-          if v[ci][cj] < 0:
-            v[ni][nj] = v[ci][cj] - 1
-            queue.append((ni, nj))
-          else:
-            v[ni][nj] = v[ci][cj] + 1
-            queue.append((ni, nj))
+      if visited[ni][nj] == 0:
+        if visited[ci][cj] < 0:
+          visited[ni][nj] = visited[ci][cj] - 1
+          queue.append((ni, nj))
         else:
-          if v[ci][cj] < 0:
-            if v[ni][nj] + v[ci][cj] - 1 == 0:
-              
+          visited[ni][nj] = visited[ci][cj] + 1
+          queue.append((ni, nj))
+      
+      else:
+        if visited[ci][cj] < 0:
+          if visited[ni][nj] + visited[ci][cj] - 1 == 0:
+            cnt += 1
+            visited[ni][nj] = 25000
+        else:
+          if visited[ni][nj] + visited[ci][cj] + 1 == 0:
+            cnt += 1
+            visited[ni][nj] = 25000
+
+  return cnt
 
 
 
-
-def dfs(n, rcnt, gcnt, tlst):
+def dfs(n, gcnt, rcnt, tlst):
   global ans
   if n == TC:
-    if rcnt == RC and gcnt == GC:
+    if G == gcnt and R == rcnt:
       ans = max(ans, bfs(tlst))
     return
-
-  dfs(n + 1, rcnt + 1, gcnt, tlst + [-1])
-  dfs(n + 1, rcnt, gcnt + 1, tlst + [1])
-  dfs(n + 1, rcnt, gcnt, tlst + [0])
-
-
+  
+  dfs(n + 1, gcnt + 1, rcnt, tlst + [1])
+  dfs(n + 1, gcnt, rcnt + 1, tlst + [-1])
+  dfs(n + 1, gcnt, rcnt, tlst + [0])
 
 
 
-N, M, RC, GC = map(int, input().split())
+
+N, M, G, R = map(int, input().split())
 arr = [[0] * (M + 2)] + [[0] + list(map(int, input().split())) + [0] for _ in range(N)] + [[0] * (M + 2)]
 
 lst = []
+
 for i in range(1, N + 1):
   for j in range(1, M + 1):
     if arr[i][j] == 2:
@@ -67,4 +75,5 @@ TC = len(lst)
 
 ans = 0
 dfs(0, 0, 0, [])
+
 print(ans)
---
+++ 비숍_1799.py
@@ -0,0 +1,79 @@
+from collections import deque
+
+def bfs(tlst):
+  queue = deque()
+  visited = [[0] * (M + 2) for _ in range(N + 2)]
+  cnt = 0
+
+  for i in range(TC):
+    if tlst[i] == 0:
+      continue
+
+    ti, tj = lst[i]
+    queue.append((ti, tj))
+    visited[ti][tj] = tlst[i]
+
+  while queue:
+    ci, cj = queue.popleft()
+    if visited[ci][cj] == 25000:
+      continue
+    
+    for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+      ni = ci + di
+      nj = cj + dj
+
+      if seeds[ni][nj] == 0 or visited[ni][nj] == 25000:
+        continue
+
+      if visited[ni][nj] == 0:                    # 처음 방문
+        if visited[ci][cj] < 0:                   # R : 1 감소
+          visited[ni][nj] = visited[ci][cj] - 1
+          queue.append((ni, nj))
+        else:                                     # G : 1 증가
+          visited[ni][nj] = visited[ci][cj] + 1
+          queue.append((ni, nj))
+
+      else:                                       # 이미 기록 -> 꽃 체크
+        if visited[ci][cj] < 0:                   # R
+          if visited[ni][nj] + visited[ci][cj] - 1 == 0:
+            cnt += 1
+            visited[ni][nj] = 25000
+
+        else:
+          if visited[ni][nj] + visited[ci][cj] + 1 == 0:
+            cnt += 1
+            visited[ni][nj] = 25000
+
+  return cnt
+
+
+def dfs(n, rcnt, gcnt, tlst):
+  global ans
+  if n == TC:                            # 모든 땅을 결정 했으면 종료
+    if rcnt == RC and gcnt == GC:
+      ans = max(ans, bfs(tlst))
+    return
+  
+  dfs(n + 1, rcnt + 1,  gcnt,      tlst + [-1])      # 빨강 씨앗
+  dfs(n + 1, rcnt,      gcnt + 1,  tlst + [1])       # 초록 씨앗
+  dfs(n + 1, rcnt,      gcnt,      tlst + [0])           # 안 뿌리는 경우
+
+
+
+
+N, M, RC, GC = map(int, input().split())
+seeds = [[0] * (M + 2)] + [[0] + list(map(int, input().split())) + [0] for _ in range(N)] + [[0] * (M + 2)]
+
+lst = []
+for i in range(1, N + 1):
+  for j in range(1, M + 1):
+    if seeds[i][j] == 2:                          # 배양액 가능한 땅이면(좌표 저장)
+      lst.append((i, j))
+
+TC = len(lst)
+
+# [1] 가능한 모든 장소에 배양액 뿌리는 방법 순회
+ans = 0
+dfs(0, 0, 0, [])
+
+print(ans)
