109508335a7d84e7acf2deeec2d8503073dcd8a6
roa_0318 <roa_0318@naver.com>
2024-03-28 21:39:46 +0900

    24.03.28

--- 비숍_1799.py
+++ 비숍_1799.py
@@ -1,79 +1,36 @@
-from collections import deque
-
-def bfs(tlst):
-  queue = deque()
-  visited = [[0] * (M + 2) for _ in range(N + 2)]
-  cnt = 0
-
-  for i in range(TC):
-    if tlst[i] == 0:
-      continue
-
-    ti, tj = lst[i]
-    queue.append((ti, tj))
-    visited[ti][tj] = tlst[i]
-
-  while queue:
-    ci, cj = queue.popleft()
-    if visited[ci][cj] == 25000:
-      continue
-    
-    for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-      ni = ci + di
-      nj = cj + dj
-
-      if seeds[ni][nj] == 0 or visited[ni][nj] == 25000:
-        continue
-
-      if visited[ni][nj] == 0:                    # 처음 방문
-        if visited[ci][cj] < 0:                   # R : 1 감소
-          visited[ni][nj] = visited[ci][cj] - 1
-          queue.append((ni, nj))
-        else:                                     # G : 1 증가
-          visited[ni][nj] = visited[ci][cj] + 1
-          queue.append((ni, nj))
-
-      else:                                       # 이미 기록 -> 꽃 체크
-        if visited[ci][cj] < 0:                   # R
-          if visited[ni][nj] + visited[ci][cj] - 1 == 0:
-            cnt += 1
-            visited[ni][nj] = 25000
-
-        else:
-          if visited[ni][nj] + visited[ci][cj] + 1 == 0:
-            cnt += 1
-            visited[ni][nj] = 25000
-
-  return cnt
-
-
-def dfs(n, rcnt, gcnt, tlst):
+def dfs(n, cnt):
   global ans
-  if n == TC:                            # 모든 땅을 결정 했으면 종료
-    if rcnt == RC and gcnt == GC:
-      ans = max(ans, bfs(tlst))
+
+  if ans >= cnt + (L - n):
     return
   
-  dfs(n + 1, rcnt + 1,  gcnt,      tlst + [-1])      # 빨강 씨앗
-  dfs(n + 1, rcnt,      gcnt + 1,  tlst + [1])       # 초록 씨앗
-  dfs(n + 1, rcnt,      gcnt,      tlst + [0])           # 안 뿌리는 경우
+  if n == L:
+    ans = max(ans, cnt)
+    return
+  
+  for i, j in lst[n]:
+    if visited[i - j] == 0:
+      visited[i - j] = 1
+      dfs(n + 1, cnt + 1)
+      visited[i - j] = 0
+  dfs(n + 1, cnt)
 
 
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
 
+lst = [[] for _ in range(2 * N)]
 
-N, M, RC, GC = map(int, input().split())
-seeds = [[0] * (M + 2)] + [[0] + list(map(int, input().split())) + [0] for _ in range(N)] + [[0] * (M + 2)]
+for i in range(N):
+  for j in range(N):
+    if arr[i][j] == 1:
+      lst[i + j].append((i, j))
 
-lst = []
-for i in range(1, N + 1):
-  for j in range(1, M + 1):
-    if seeds[i][j] == 2:                          # 배양액 가능한 땅이면(좌표 저장)
-      lst.append((i, j))
+L = (2 * N) - 1
+visited = [0] * (2 * N)
 
-TC = len(lst)
-
-# [1] 가능한 모든 장소에 배양액 뿌리는 방법 순회
 ans = 0
-dfs(0, 0, 0, [])
+
+dfs(0, 0)
 
 print(ans)
