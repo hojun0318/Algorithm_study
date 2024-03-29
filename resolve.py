9ae89f4ab61a39ffac8d40710bd539188f81a290
roa_0318 <roa_0318@naver.com>
2024-03-29 19:08:04 +0900

    24.03.29

---
+++ resolve.py
@@ -0,0 +1,18 @@
+left = list(input())
+right = []
+
+for _ in range(int(input())):
+  cursor = list(input().split())
+
+  if cursor[0] == 'L' and left:
+    right.append(left.pop())
+  elif cursor[0] == 'D' and right:
+    left.append(right.pop())
+  elif cursor[0] == 'B' and left:
+    left.pop()
+  elif cursor[0] == 'P':
+    left.append(cursor[1])
+
+ans = left + right[::-1]
+
+print(''.join(ans))
--- 비숍_1799.py
+++ 비숍_1799.py
@@ -1,19 +1,19 @@
 def dfs(n, cnt):
   global ans
 
-  if ans >= cnt + (L - n):
+  if ans >= (cnt + (L + 1 - n) // 2):
     return
   
-  if n == L:
+  if n >= L:
     ans = max(ans, cnt)
     return
   
   for i, j in lst[n]:
     if visited[i - j] == 0:
       visited[i - j] = 1
-      dfs(n + 1, cnt + 1)
+      dfs(n + 2, cnt + 1)
       visited[i - j] = 0
-  dfs(n + 1, cnt)
+  dfs(n + 2, cnt)
 
 
 N = int(input())
@@ -30,7 +30,8 @@ L = (2 * N) - 1
 visited = [0] * (2 * N)
 
 ans = 0
-
 dfs(0, 0)
+t = ans
+dfs(1, 0)
 
-print(ans)
+print(ans + t)
