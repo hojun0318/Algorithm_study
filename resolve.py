747036300c43f61436111d706581e34a9ca194ca
roa_0318 <roa_0318@naver.com>
2024-04-01 20:21:43 +0900

    24.04.01

--- resolve.py
+++ resolve.py
@@ -1,27 +1,37 @@
 from collections import deque
 
-N, M = map(int, input().split())
-stk = list(map(int, input().split()))
+T = int(input())
 
-queue = deque()
-for i in range(1, N + 1):
-  queue.append(i)
+for _ in range(T):
+  p = list(map(str, input()))
+  n = int(input())
+  lst = input().rstrip()[1:-1].split(",")
+  
+  if n != 0:
+    queue = deque(lst)
+  else:
+    queue = deque([])
 
-ans = 0
+  flagError = False
+  cnt = 0
 
-for num in stk:
-  while True:
-    if queue[0] == num:
-      queue.popleft()
-      break
+  for c in p:
+    if c == 'R':
+      cnt += 1
     else:
-      if queue.index(num) < len(queue) / 2:
-        while queue[0] != num:
-          queue.append(queue.popleft())
-          ans += 1
+      if len(queue) == 0:
+        print('error')
+        flagError = True
+        break
       else:
-        while queue[0] != num:
-          queue.appendleft(queue.pop())
-          ans += 1
-          
-print(ans)
+        if cnt % 2 == 0:
+          queue.popleft()
+        else:
+          queue.pop()
+
+  if not flagError:
+    if cnt % 2 == 0:
+      print("[" + ",".join(queue) + "]")
+    else:
+      queue = deque(reversed(queue))
+      print("[" + ",".join(queue) + "]")
