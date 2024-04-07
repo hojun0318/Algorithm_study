7d87324ad081ad831777513929da2253a92ad226
roa_0318 <roa_0318@naver.com>
2024-04-07 13:59:12 +0900

    24.04.07

--- resolve.py
+++ resolve.py
@@ -1,57 +1,20 @@
 from collections import deque
 
-R, C = map(int, input().split())
-maps = [list(map(str, input())) for _ in range(R)]
+N, K = map(int, input().split())
+visited = [0] * 100001
 
-fire_visited = [[0] * C for _ in range(R)]
-jihun_visited = [[0] * C for _ in range(R)]
+queue = deque()
+queue.append(N)
+visited[N] = 1
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
+while queue:
+  x = queue.popleft()
 
-def fire_bfs(x, y):
-  fire_queue = deque()
-  fire_queue.append((x, y))
+  if x == K:
+    print(visited[x] - 1)
+    break
 
-  while fire_queue:
-    x, y = fire_queue.popleft()
-
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-
-      if 0 <= nx < R and 0 <= ny < C:
-        if not fire_visited[nx][ny] and maps[nx][ny] != '#':
-          fire_queue.append((nx, ny))
-          fire_visited[nx][ny] = fire_visited[x][y] + 1
-
-def jihun_bfs(x, y):
-  jihun_queue = deque()
-  jihun_queue.append((x, y))
-
-  while jihun_queue:
-    x, y = jihun_queue.popleft()
-
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      
-      if 0 <= nx < R and 0 <= ny < C:
-        if maps[nx][ny] != '#' and not jihun_visited[nx][ny]:
-          if jihun_visited[x][y] + 1 < fire_visited[nx][ny] or not fire_visited[nx][ny]:
-            jihun_queue.append((nx, ny))
-            jihun_visited[nx][ny] = jihun_visited[x][y] + 1
-      else:
-        return jihun_visited[x][y]
-        
-  return "IMPOSSIBLE"
-
-
-for i in range(R):
-  for j in range(C):
-    if maps[i][j] == 'F':
-      fire_visited[i][j] = 1
-      fire_bfs(i, j)
-    if maps[i][j] == 'J':
-      jihun_visited[i][j] = 1
-      print(jihun_bfs(i, j))
+  for nx in (x + 1, x - 1, x * 2):
+    if 0 <= nx <= 100000 and not visited[nx]:
+      visited[nx] = visited[x] + 1
+      queue.append(nx)
