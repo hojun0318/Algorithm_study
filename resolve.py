bc949dbdbb781c7e5666e70363721a9b1593a4b1
roa_0318 <roa_0318@naver.com>
2024-04-11 22:01:40 +0900

    '24.04.11'

--- resolve.py
+++ resolve.py
@@ -1,25 +1,38 @@
 from collections import deque
 
-F, S, G, U, D = map(int, input().split())
-visited = [0] * 1000001
+def bfs(x, y, c):
+  queue = deque()
+  queue.append((x, y, c))
+  visited[x][y][c] = 1
 
-queue = deque()
-queue.append(S)
-visited[S] = 1
-flag = False
+  while queue:
+    x, y, c = queue.popleft()
 
-while queue:
-  s = queue.popleft()
+    if x == (N - 1) and y == (M - 1):
+      return visited[x][y][c]
+      
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
 
-  if s == G:
-    flag = True
-    print(visited[s] - 1)
-    break
+      if 0 <= nx < N and 0 <= ny < M:
+        if maps[nx][ny] == 0 and not visited[nx][ny][c]:
+          visited[nx][ny][c] = visited[x][y][c] + 1
+          queue.append((nx, ny, c))
+        if maps[nx][ny] == 1 and c == 1 and not visited[nx][ny][c - 1]:
+          visited[nx][ny][c - 1] = visited[x][y][c] + 1
+          queue.append((nx, ny, c - 1))
 
-  for ns in (s + U, s - D):
-    if 1 <= ns <= F and not visited[ns]:
-      visited[ns] = visited[s] + 1
-      queue.append(ns)
+  
+  return -1
 
-if flag == False:
-  print('use the stairs')
+
+
+N, M = map(int, input().split())
+maps = [list(map(int, input())) for _ in range(N)]
+visited = [[[0] * 2 for _ in range(M)] for _ in range(N)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+print(bfs(0, 0, 1))
