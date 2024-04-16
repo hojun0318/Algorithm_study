3fc5c5b5c8fd99e26acca7fa1006172573bfee96
roa_0318 <roa_0318@naver.com>
2024-04-16 19:14:01 +0900

    24.04.16

--- resolve.py
+++ resolve.py
@@ -1,76 +1,69 @@
 from collections import deque
 
-N, M = map(int, input().split())
-icebergs = [list(map(int, input().split())) for _ in range(N)]
-
-queue = deque()
+N = int(input())
+maps = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0] * N for _ in range(N)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-for i in range(N):
-  for j in range(M):
-    if icebergs[i][j]:
-      queue.append((i, j))
-
 cnt = 0
-flag = True
+ans = 100001
 
-while queue:
-  next = []
-  melts = []
+def bfs(x, y):
+  queue = deque()
+  queue.append((x, y))
+  maps[x][y] = cnt
+  visited[x][y] = 1
 
-  # 녹는 양 계산
-  for x, y in queue:
-    melt = 0
-
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-
-      if 0 <= nx < N and 0 <= ny < M and icebergs[nx][ny] <= 0:
-        melt += 1
-    melts.append(melt)
-
-  for k in range(len(queue)):
-    r = queue[k][0]
-    c = queue[k][1]
-    icebergs[r][c] -= melts[k]
-
-    if icebergs[r][c] > 0:
-      next.append((r, c))
-
-  # 빙하가 남은 위치 다시 큐에 삽입
-  queue = next[:]
-
-  if not next:
-    break
-
-  ice = deque([next[0]])
-  ice_cnt = 1
-  visited = [[0] * M for _ in range(N)]
-  visited[next[0][0]][next[0][1]] = 1
-
-  while ice:
-    x, y = ice.popleft()
+  while queue:
+    x, y = queue.popleft()
 
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
 
-      if 0 <= nx < N and 0 <= ny < M and icebergs[nx][ny] > 0 and not visited[nx][ny]:
+      if 0 <= nx < N and 0 <= ny < N and maps[nx][ny] == 1 and not visited[nx][ny]:
+        maps[nx][ny] = cnt
+        queue.append((nx, ny))
         visited[nx][ny] = 1
-        ice.append((nx, ny))
-        ice_cnt += 1
-  
-  # 소요 기간 계산
-  cnt += 1
 
-  if ice_cnt != len(queue):
-    flag = False
-    break
 
-if flag:
-  print(0)
-else:
-  print(cnt)
+def load_bfs(v):
+  queue = deque()
+  visited = [[-1] * N for _ in range(N)]
+
+  for i in range(N):
+    for j in range(N):
+      if maps[i][j] == v:
+        visited[i][j] = 0
+        queue.append((i, j))
+
+  while queue:
+    x, y = queue.popleft()
+
+    for k in range(4):
+      nx = x + dx[k]
+      ny = y + dy[k]
+
+      if 0 <= nx < N and 0 <= ny < N:
+        if maps[nx][ny] and maps[nx][ny] != v:
+          return visited[x][y]
+        
+        if not maps[nx][ny] and visited[nx][ny] == -1:
+          visited[nx][ny] = visited[x][y] + 1
+          queue.append((nx, ny))
+
+  return 10001
+
+for i in range(N):
+  for j in range(N):
+    if maps[i][j] == 1 and not visited[i][j]:
+      cnt += 1
+      bfs(i, j)
+
+
+for v in range(1, (cnt + 1)):
+  ans = min(ans, load_bfs(v))
+
+print(ans)
