e5a7d04ac3be6d1f5747ca19af9d2242a3db858f
roa_0318 <roa_0318@naver.com>
2024-04-18 23:54:03 +0900

    24.04.18

--- resolve.py
+++ resolve.py
@@ -1,35 +1,45 @@
 from collections import deque
 
-MAX = 100001
-visited = [0] * (MAX)
-N, K = map(int, input().split())
-
-
-def bfs(x, y):
+def bfs(x, y, h):
   queue = deque()
-
-  if x == 0:
-    queue.append(1)
-  else:
-    queue.append(x)
+  queue.append((x, y, h))
 
   while queue:
-    x = queue.popleft()
+    x, y, h = queue.popleft()
 
-    if y == x:
-      return visited[x]
+    if x == (H - 1) and y == (W - 1):
+      return visited[x][y][h]
     
-    for nx in (x - 1, x + 1, x * 2):
-      if 0 <= nx < MAX and visited[nx] == 0:
-        if nx == x * 2:
-          visited[nx] = visited[x]
-          queue.appendleft(nx)
-        else:
-          visited[nx] = visited[x] + 1
-          queue.append(nx)
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
 
-if N == 0:
-  print(bfs(N, K) + 1)
+      if 0 <= nx < H and 0 <= ny < W and not visited[nx][ny][h] and maps[nx][ny] == 0:
+        queue.append((nx, ny, h))
+        visited[nx][ny][h] = visited[x][y][h] + 1
+    
+    if h < K:
+      for j in range(8):
+        nx = x + hx[j]
+        ny = y + hy[j]
 
-else:
-  print(bfs(N, K))
+        if 0 <= nx < H and 0 <= ny < W and not visited[nx][ny][h + 1] and maps[nx][ny] == 0:
+          queue.append((nx, ny, h + 1))
+          visited[nx][ny][h + 1] = visited[x][y][h] + 1
+
+  return - 1
+
+
+
+K = int(input())
+W, H = map(int, input().split())
+maps = [list(map(int, input().split())) for _ in range(H)]
+visited = [[[0] * (K + 1) for _ in range(W)] for _ in range(H)]
+
+hx = [-1, -2, -2, -1, 1, 2, 2, 1]
+hy = [2, 1, -1, -2, -2, -1, 1, 2]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+print(bfs(0, 0, 0))
