c2f38c7dcae03a5ca67fb014114ed6205504136d
roa_0318 <roa_0318@naver.com>
2024-04-19 23:05:35 +0900

    24.04.19

--- resolve.py
+++ resolve.py
@@ -1,45 +1,35 @@
 from collections import deque
 
-def bfs(x, y, h):
+N, K = map(int, input().split())
+
+visited = [0] * 100001
+node = [0] * 100001
+
+def path(x):
+  lst = []
+  tmp = x
+  for _ in range(visited[x] + 1):
+    lst.append(tmp)
+    tmp = node[tmp]
+  print(' '.join(map(str, lst[::-1])))
+
+def bfs():
   queue = deque()
-  queue.append((x, y, h))
+  queue.append(N)
 
   while queue:
-    x, y, h = queue.popleft()
+    s = queue.popleft()
 
-    if x == (H - 1) and y == (W - 1):
-      return visited[x][y][h]
-    
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-
-      if 0 <= nx < H and 0 <= ny < W and not visited[nx][ny][h] and maps[nx][ny] == 0:
-        queue.append((nx, ny, h))
-        visited[nx][ny][h] = visited[x][y][h] + 1
-    
-    if h < K:
-      for j in range(8):
-        nx = x + hx[j]
-        ny = y + hy[j]
-
-        if 0 <= nx < H and 0 <= ny < W and not visited[nx][ny][h + 1] and maps[nx][ny] == 0:
-          queue.append((nx, ny, h + 1))
-          visited[nx][ny][h + 1] = visited[x][y][h] + 1
-
-  return - 1
+    if s == K:
+      print(visited[s])
+      path(s)
+      return s
+  
+    for ns in (s - 1, s + 1, s * 2):
+      if 0 <= ns < 100001 and not visited[ns]:
+        visited[ns] = visited[s] + 1
+        queue.append(ns)
+        node[ns] = s
 
 
-
-K = int(input())
-W, H = map(int, input().split())
-maps = [list(map(int, input().split())) for _ in range(H)]
-visited = [[[0] * (K + 1) for _ in range(W)] for _ in range(H)]
-
-hx = [-1, -2, -2, -1, 1, 2, 2, 1]
-hy = [2, 1, -1, -2, -2, -1, 1, 2]
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-print(bfs(0, 0, 0))
+bfs()
