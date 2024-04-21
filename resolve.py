617b71864ba329b8531f6a5732185022a6d7091e
roa_0318 <roa_0318@naver.com>
2024-04-21 22:16:51 +0900

    24.04.21

--- resolve.py
+++ resolve.py
@@ -2,37 +2,45 @@ from collections import deque
 
 N, M, K = map(int, input().split())
 matrix = [list(map(int, input())) for _ in range(N)]
-visited = [[[0] * (K + 1) for _ in range(M)] for _ in range(N)]
-visited[0][0][0] = 1
+visited = [[K + 1 for _ in range(M)] for _ in range(N)]
+visited[0][0] = 0
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(x, y, k):
+def bfs(start):
   queue = deque()
-  queue.append((x, y, k))
+  queue.append(start)
+  ans = 1
+  time = True
 
   while queue:
-    x, y, k = queue.popleft()
+    for _ in range(len(queue)):
+      x, y, k = queue.popleft()
 
-    if x == (N - 1) and y == (M - 1):
-      return visited[x][y][k]
+      if x == (N - 1) and y == (M - 1):
+        print(ans)
+        return
+      
+      for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] > k:
+          if matrix[nx][ny] == 0:
+            queue.append((nx, ny, k))
+            visited[nx][ny] = k
+          elif k < K:
+            if not time: 
+              queue.append((x, y, k))
+            else: 
+              visited[nx][ny] = k
+              queue.append((nx, ny, k + 1))
 
-      if 0 <= nx < N and 0 <= ny < M:
-        if visited[nx][ny][k] == 0 and matrix[nx][ny] == 0:
-          queue.append((nx, ny, k))
-          visited[nx][ny][k] = visited[x][y][k] + 1
+    ans += 1
+    time = not time
 
-        if k < K:
-          if matrix[nx][ny] == 1 and visited[nx][ny][k + 1] == 0:
-            queue.append((nx, ny, k + 1))
-            visited[nx][ny][k + 1] = visited[x][y][k] + 1
-  
-  return -1
+  print(-1)
+  return
 
-
-print(bfs(0, 0, 0))
+bfs((0, 0, 0))
