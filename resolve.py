a303559628f2470393c62226df3a7a6b9cf195fc
roa_0318 <roa_0318@naver.com>
2024-04-26 23:48:30 +0900

    24.04.26

--- resolve.py
+++ resolve.py
@@ -1,10 +1,11 @@
-N = int(input())
+A, B, C = map(int, input().split())
 
-obN = list(bin(N)[2:][::-1])
-t, ans = 1, 0
-
-for i in obN:
-  ans += int(i) * t
-  t *= 3
-
-print(ans)
+def POW(a, b, c):
+  if b == 1:
+    return a % c
+  elif b % 2 == 0:
+    return (POW(a, b // 2, c) ** 2) % C
+  else:
+    return ((POW(a, b // 2, c) ** 2) * a) % C
+  
+print(POW(A, B, C))
