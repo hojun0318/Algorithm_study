a51a24cafbe65257d177378f67caf787c771c727
roa_0318 <roa_0318@naver.com>
2024-04-29 23:55:34 +0900

    24.04.29

--- resolve.py
+++ resolve.py
@@ -1,15 +1,25 @@
-N = int(input())
+def zet(n, r, c):
+    global res
 
-def hanoi(n, start, end):
-  if n == 1:
-    print(start, end)
-    return
-  
-  hanoi(n - 1, start, 6 - start - end)  # 1단계
-  print(start, end) # 2단계
-  hanoi(n - 1, 6 - start - end, end)  # 3단계
+    # 찾는 좌표라면 res를 출력하고 종료
+    if r == R and c == C:
+        print(int(res))
+        exit(0)
+
+    # 탐색 증인 배열 중에 찾는 좌표가 없다면 좌표에 크기를 더한다.
+    if not (r <= R < r + n and c <= C < c + n):
+        res += n * n
+        return
+
+    # 1/2/3/4사분면을 재귀적으로 탐색
+    zet(n/2, r, c) # 1사분면
+    zet(n/2, r, c + n/2) # 2사분면
+    zet(n/2, r + n/2, c) # 3사분면
+    zet(n/2, r + n/2, c + n/2) # 4사분면
 
 
+N, R, C = map(int, input().split())
+res = 0
 
-print(2 ** N - 1)
-hanoi(N, 1, 3)
+# 2^n을 0, 0부터 탐색
+zet(2 ** N, 0, 0)
