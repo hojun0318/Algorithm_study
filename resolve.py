114ad120674e52887da75a9aeed6a4aa7111a43d
roa_0318 <roa_0318@naver.com>
2024-05-02 23:01:39 +0900

    24.05.02

--- resolve.py
+++ resolve.py
@@ -1,31 +1,19 @@
-N = int(input())
-board = [list(map(int, input().split())) for _ in range(N)]
-
-result_minus = 0
-result_zero = 0
-result_plus = 0
-
-def dfs(x, y, n):
-  global result_minus, result_zero, result_plus
-  num_check = board[x][y]
+def dfs(n, sm, cnt):
+  global ans
+  if n == N:
+    if sm == S and cnt > 0:
+      ans += 1
+    return
   
-  for i in range(x, x + n):
-    for j in range(y, y + n):
-      if board[i][j] != num_check:
-        for k in range(3):
-          for l in range(3):
-            dfs(x + k * n // 3, y + l * n // 3, n // 3)
-        return
-      
-  if num_check == -1:
-    result_minus += 1
-  elif num_check == 0:
-    result_zero += 1
-  else:
-    result_plus += 1
+  dfs(n + 1, sm + lst[n], cnt + 1)
+  dfs(n + 1, sm, cnt)
 
-dfs(0, 0, N)
 
-print(result_minus)
-print(result_zero)
-print(result_plus)
+N, S = map(int, input().split())
+lst = list(map(int, input().split()))
+
+ans = 0
+
+dfs(0, 0, 0)
+
+print(ans)
