00751ee5c7b248c775b8b86f08594359cc9a42fa
roa_0318 <roa_0318@naver.com>
2024-06-10 06:32:11 +0900

    24.06.10

--- resolve.py
+++ resolve.py
@@ -1,52 +1,64 @@
-def dfs(n, lst):
-  global cnt, tmp
-  if n == L and cnt > 0 and tmp > 1:
-    ans.append(lst)
+from collections import deque
+
+def bfs(x, y):
+  queue = deque()
+  bfs_visited = [[0] * 5 for _ in range(5)]
+  queue.append((x, y))
+  bfs_visited[x][y] = 1
+  cnt = 1
+
+  while queue:
+    x, y = queue.popleft()
+
+    for d in range(4):
+      nx = x + dx[d]
+      ny = y + dy[d]
+
+      if 0 <= nx < 5 and 0 <= ny < 5:
+        if not bfs_visited[nx][ny] and visited[nx][ny] == 1:
+          bfs_visited[nx][ny] = 1
+          cnt += 1
+          queue.append((nx, ny))
+
+  return cnt == 7
+
+
+def check():
+  for i in range(5):
+    for j in range(5):
+      if visited[i][j] == 1:
+        return bfs(i, j)
+
+
+def dfs(n, cnt, scnt):
+  global ans
+
+  if cnt > 7:
     return
   
-  for i in range(C):
-    if lst:
-      if not visited[i] and lst[-1] < st[i]:
-        if st[i] in mohmm:
-          cnt += 1
-          visited[i] = 1
-          dfs(n + 1, lst + [st[i]])
-          visited[i] = 0
-          cnt -= 1
-        else:
-          tmp += 1
-          visited[i] = 1
-          dfs(n + 1, lst + [st[i]])
-          visited[i] = 0
-          tmp -= 1
-    else:
-      if not visited[i]:
-        if st[i] in mohmm:
-          cnt += 1
-          visited[i] = 1
-          dfs(n + 1, lst + [st[i]])
-          visited[i] = 0
-          cnt -= 1
-        else:
-          tmp += 1
-          visited[i] = 1
-          dfs(n + 1, lst + [st[i]])
-          visited[i] = 0
-          tmp -= 1
+  if n == 25:
+    if cnt == 7 and scnt >= 4:
+      if check():
+        ans += 1
+    
+    return
+  
+  visited[n // 5][n % 5] = 1
+  dfs(n + 1, cnt + 1, scnt + int(arr[n // 5][n % 5] == 'S'))
+  visited[n // 5][n % 5] = 0
+
+  dfs(n + 1, cnt, scnt)
 
 
-L, C = map(int, input().split())
-st = sorted(list(map(str, input().split())))
 
-mohmm = ['a', 'e', 'i', 'o', 'u']
-cnt = tmp = 0
-ans = []
-visited = [0] * C
+arr= [list(map(str, input())) for _ in range(5)]
+ans = 0
+visited = [[0] * 5 for _ in range(5)]
 
-dfs(0, [])
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-for lst in ans:
-  res = ''
-  for s in lst:
-    res += s
-  print(res)
+
+dfs(0, 0, 0)
+
+print(ans)
