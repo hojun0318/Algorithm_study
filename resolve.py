067c54fb4a9d7ed7fe1c67fdc58e92990ab5fd7c
roa_0318 <roa_0318@naver.com>
2024-06-11 06:11:39 +0900

    24.06.11

--- resolve.py
+++ resolve.py
@@ -1,33 +1,52 @@
 def dfs(n, lst):
-  if n == 6:
+  global cnt, tmp
+  if n == L and cnt > 0 and tmp > 1:
     ans.append(lst)
     return
   
-  for i in range(k):
+  for i in range(C):
     if lst:
-      if not visited[i] and lst[-1] < S[i]:
-        visited[i] = 1
-        dfs(n + 1, lst + [S[i]])
-        visited[i] = 0
+      if not visited[i] and lst[-1] < st[i]:
+        if st[i] in mohmm:
+          cnt += 1
+          visited[i] = 1
+          dfs(n + 1, lst + [st[i]])
+          visited[i] = 0
+          cnt -= 1
+        else:
+          tmp += 1
+          visited[i] = 1
+          dfs(n + 1, lst + [st[i]])
+          visited[i] = 0
+          tmp -= 1
     else:
       if not visited[i]:
-        visited[i] = 1
-        dfs(n + 1, lst + [S[i]])
-        visited[i] = 0
+        if st[i] in mohmm:
+          cnt += 1
+          visited[i] = 1
+          dfs(n + 1, lst + [st[i]])
+          visited[i] = 0
+          cnt -= 1
+        else:
+          tmp += 1
+          visited[i] = 1
+          dfs(n + 1, lst + [st[i]])
+          visited[i] = 0
+          tmp -= 1
 
 
-while True:
-  lst = list(map(int, input().split()))
-  k = lst[0]
-  S = lst[1:]
+L, C = map(int, input().split())
+st = sorted(list(map(str, input().split())))
 
-  if k == 0:
-    break
-  
-  ans = []
-  visited = [0] * k
-  dfs(0, [])
+mohmm = ['a', 'e', 'i', 'o', 'u']
+cnt = tmp = 0
+ans = []
+visited = [0] * C
 
-  for lst in ans:
-    print(*lst)
-  print()
+dfs(0, [])
+
+for lst in ans:
+  res = ''
+  for s in lst:
+    res += s
+  print(res)
