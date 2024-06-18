77920b53523353580d83dd4693c63c4c9fb40965
roa_0318 <roa_0318@naver.com>
2024-06-18 10:22:20 +0900

    24.06.18

--- resolve.py
+++ resolve.py
@@ -1,14 +1,15 @@
 def dfs(n, lst):
   if n == M:
-    ans.append(lst)
-    return
-  
+    lst.sort()
+    if lst not in ans:
+      ans.append(lst)
+      return
+    
   for i in range(N):
     if not visited[i]:
       visited[i] = 1
       dfs(n + 1, lst + [nums[i]])
       visited[i] = 0
-
 
 N, M = map(int, input().split())
 nums = list(map(int, input().split()))
