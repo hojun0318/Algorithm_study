31348c67a81af31bc6191bd50db886f0f9ee9779
roa_0318 <roa_0318@naver.com>
2024-06-19 23:25:27 +0900

    24.06.19

--- resolve.py
+++ resolve.py
@@ -4,44 +4,32 @@
 from collections import deque
 
 def bfs(x, y):
-  global tmp
   queue = deque()
   queue.append((x, y))
+  visited[x][y] = 1
 
   while queue:
     x, y = queue.popleft()
-    tmp += 1
+    if x == N - 1 and y == M - 1:
+      return visited[N - 1][M - 1]
 
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
 
-      if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and art[nx][ny] == 1:
-        visited[nx][ny] = 1
-        queue.append((nx, ny))
-
-  return tmp
-
-
-
+      if 0 <= nx < N and 0 <= ny < M:
+        if not visited[nx][ny] and maps[nx][ny] == 1:
+          visited[nx][ny] = visited[x][y] + 1
+          queue.append((nx, ny))
+  
+  return 0
 
 
-
-n, m = map(int, input().split())
-art = [list(map(int, input().split())) for _ in range(n)]
-cnt = ans = tmp = 0 
-visited = [[0] * m for _ in range(n)]
+N, M = map(int, input().split())
+maps = [list(map(int, input())) for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, -1, 1]
 
-for i in range(n):
-  for j in range(m):
-    if not visited[i][j] and art[i][j] == 1:
-      visited[i][j] = 1
-      cnt += 1
-      ans = max(ans, bfs(i, j))
-      tmp = 0
-
-print(cnt)
-print(ans)
+print(bfs(0, 0))
