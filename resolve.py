e97f75706a54415926d9c1e0ea4fe1b735a18c07
roa_0318 <roa_0318@naver.com>
2024-06-20 22:37:19 +0900

    24.06.20

--- resolve.py
+++ resolve.py
@@ -3,33 +3,38 @@
 
 from collections import deque
 
-def bfs(x, y):
+def bfs():
+  ans = 0
   queue = deque()
-  queue.append((x, y))
-  visited[x][y] = 1
+  for i in range(N):
+    for j in range(M):
+      if boxs[i][j] == 1:
+        queue.append((i, j))
 
   while queue:
     x, y = queue.popleft()
-    if x == N - 1 and y == M - 1:
-      return visited[N - 1][M - 1]
 
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
+    for d in range(4):
+      nx = x + dx[d]
+      ny = y + dy[d]
 
       if 0 <= nx < N and 0 <= ny < M:
-        if not visited[nx][ny] and maps[nx][ny] == 1:
-          visited[nx][ny] = visited[x][y] + 1
+        if boxs[nx][ny] == 0:
           queue.append((nx, ny))
-  
-  return 0
+          boxs[nx][ny] = boxs[x][y] + 1
 
+  for lst in boxs:
+    for chk in lst:
+      if chk == 0:
+        return -1
+      ans = max(ans, chk)
 
-N, M = map(int, input().split())
-maps = [list(map(int, input())) for _ in range(N)]
-visited = [[0] * M for _ in range(N)]
+  return ans - 1
 
-dx = [1, -1, 0, 0]
+M, N = map(int, input().split())
+boxs = [list(map(int, input().split())) for _ in range(N)]
+
+dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-print(bfs(0, 0))
+print(bfs())
