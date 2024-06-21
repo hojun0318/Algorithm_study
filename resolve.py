6d9902b5081e77e8fcfe79d8f7a9513069995ebe
roa_0318 <roa_0318@naver.com>
2024-06-21 22:12:10 +0900

    24.06.21

--- resolve.py
+++ resolve.py
@@ -3,63 +3,44 @@
 
 from collections import deque
 
-def bfs():
+def bfs(x, y):
   queue = deque()
-
-  for x in range(R):
-    for y in range(C):
-      if load[x][y] == 'F':
-        fire_visited[x][y] = 1
-        queue.append((x, y, 'f'))
-
-  for r in range(R):
-    for c in range(C):
-      if load[r][c] == 'J':
-        jihun_visited[r][c] = 1
-        queue.append((r, c, 'j'))
+  queue.append((x, y))
+  maps[x][y] = 0
 
   while queue:
-    x, y, s = queue.popleft()
-    if s == 'j':
-      if x == (R - 1) or x == 0 or y == 0 or y ==(C - 1):
-        return jihun_visited[x][y]
-      
-    
+    x, y = queue.popleft()
+
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
 
-      if s == 'j':
-        if 0 <= nx < R and 0 <= ny < C:
-          if load[nx][ny] == '.':
-            if not jihun_visited[nx][ny] and not fire_visited[nx][ny]:
-              jihun_visited[nx][ny] = jihun_visited[x][y] + 1
-              queue.append((nx, ny, 'j'))
-
-      if s == 'f':
-        if 0 <= nx < R and 0 <= ny < C:
-          if load[nx][ny] == '.' or load[nx][ny] == 'J':
-            if not fire_visited[nx][ny]:
-              fire_visited[nx][ny] = fire_visited[x][y] + 1
-              queue.append((nx, ny, 'f'))
-
-  return "IMPOSSIBLE"
+      if 0 <= nx < M and 0 <= ny < N:
+        if maps[nx][ny] == 1:
+          maps[nx][ny] = 0
+          queue.append((nx, ny))
 
 
-R, C = map(int, input().split())
-load = [list(map(str, input())) for _ in range(R)]
-jihun_visited = [[0] * C for _ in range(R)]
-fire_visited = [[0] * C for _ in range(R)]
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
 
-print(bfs())
+T = int(input())
+for _ in range(T):
+  M, N, K = map(int, input().split())
+  maps = [[0] * N for _ in range(M)]
 
+  for _ in range(K):
+    x, y = map(int, input().split())
+    maps[x][y] = 1
 
-for i in fire_visited:
-  print(i)
-print('-----------------')
-for i in jihun_visited:
-  print(i)
+  ans = 0
+
+  dx = [-1, 1, 0, 0]
+  dy = [0, 0, -1, 1]
+
+  for i in range(M):
+    for j in range(N):
+      if maps[i][j] == 1:
+        ans += 1
+        bfs(i, j)
+
+  print(ans)
