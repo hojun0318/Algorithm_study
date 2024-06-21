e7a826b08e3184594b1d96d1867bc349f0dcc429
roa_0318 <roa_0318@naver.com>
2024-06-22 00:50:33 +0900

    24.06.22

--- resolve.py
+++ resolve.py
@@ -3,44 +3,52 @@
 
 from collections import deque
 
-def bfs(x, y):
-  queue = deque()
-  queue.append((x, y))
-  maps[x][y] = 0
-
-  while queue:
-    x, y = queue.popleft()
-
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-
-      if 0 <= nx < M and 0 <= ny < N:
-        if maps[nx][ny] == 1:
-          maps[nx][ny] = 0
-          queue.append((nx, ny))
-
-
-
-
-T = int(input())
-for _ in range(T):
-  M, N, K = map(int, input().split())
-  maps = [[0] * N for _ in range(M)]
-
-  for _ in range(K):
-    x, y = map(int, input().split())
-    maps[x][y] = 1
-
+def bfs():
   ans = 0
+  queue = deque()
+  
+  for k in range(H):
+    for i in range(N):
+      for j in range(M):
+        if boxs[k][i][j] == 1:
+          queue.append((k, i, j))
+          
+  while queue:
+    z, x, y = queue.popleft()
 
-  dx = [-1, 1, 0, 0]
-  dy = [0, 0, -1, 1]
+    for v in range(2):
+      nz = z + dz[v]
 
-  for i in range(M):
-    for j in range(N):
-      if maps[i][j] == 1:
-        ans += 1
-        bfs(i, j)
+      if 0 <= nz < H:
+        if boxs[nz][x][y] == 0:
+          boxs[nz][x][y] = boxs[z][x][y] + 1
+          queue.append((nz, x, y))
 
-  print(ans)
+    for c in range(4):
+      nx = x + dx[c]
+      ny = y + dy[c]
+
+      if 0 <= nx < N and 0 <= ny < M:
+        if boxs[z][nx][ny] == 0:
+          boxs[z][nx][ny] = boxs[z][x][y] + 1
+          queue.append((z, nx, ny))
+
+
+  for h in range(H):
+    for n in range(N):
+      for m in range(M):
+        if boxs[h][n][m] == 0:
+          return -1
+        ans = max(ans, boxs[h][n][m])
+
+  return ans - 1
+
+
+M, N, H = map(int, input().split())
+boxs = [[list(map(int,input().split())) for _ in range(N)] for _ in range(H)]
+
+dz = [-1, 1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+print(bfs())
