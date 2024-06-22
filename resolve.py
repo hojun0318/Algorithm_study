50738cb1c12ea63927b2b6b24732952a40c2a7aa
roa_0318 <roa_0318@naver.com>
2024-06-22 18:14:16 +0900

    24.06.22

--- resolve.py
+++ resolve.py
@@ -3,32 +3,31 @@
 
 from collections import deque
 
-def bfs(sx, sy):
+def bfs():
   queue = deque()
-  queue.append((sx, sy))
-  visited[sx][sy] = 1
+  queue.append(S)
+  visited[S] = 1
 
   while queue:
-    x, y = queue.popleft()
+    s = queue.popleft()
+    if s > G and D == 0:
+      continue
 
-    if x == ex and y == ey:
-      return visited[x][y] - 1
+    if s == G:
+      return visited[s] - 1
+    
+    for ns in [(s + U), (s - D)]:
+      if 1 <= ns < (F + 1):
+        if not visited[ns]:
+          visited[ns] = visited[s] + 1
+          queue.append((ns))
 
-    for i, j in [(-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]:
-      nx = x + i
-      ny = y + j
-
-      if 0 <= nx < I and 0 <= ny < I:
-        if not visited[nx][ny]:
-          visited[nx][ny] = visited[x][y] + 1
-          queue.append((nx, ny))
+  return "use the stairs"
 
 
-T = int(input())
-for _ in range(T):
-  I = int(input())
-  sx, sy = map(int, input().split())
-  ex, ey = map(int, input().split())
-  visited = [[0] * I for _ in range(I)]
+F, S, G, U, D = map(int, input().split())
+visited = [0] * (F + 1)
 
-  print(bfs(sx, sy))
+print(bfs())
+
+print(visited)
