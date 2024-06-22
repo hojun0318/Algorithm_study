355cd208b678d443a6e591af1fb325eefe7d92d9
roa_0318 <roa_0318@naver.com>
2024-06-22 18:39:22 +0900

    24.06.22

--- resolve.py
+++ resolve.py
@@ -3,31 +3,46 @@
 
 from collections import deque
 
-def bfs():
+def bfs(x, y):
   queue = deque()
-  queue.append(S)
-  visited[S] = 1
+  queue.append((x, y))
 
   while queue:
-    s = queue.popleft()
-    if s > G and D == 0:
-      continue
+    x, y = queue.popleft()
 
-    if s == G:
-      return visited[s] - 1
-    
-    for ns in [(s + U), (s - D)]:
-      if 1 <= ns < (F + 1):
-        if not visited[ns]:
-          visited[ns] = visited[s] + 1
-          queue.append((ns))
-
-  return "use the stairs"
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
 
 
-F, S, G, U, D = map(int, input().split())
-visited = [0] * (F + 1)
+      if 0 <= nx < N and 0 <= ny < N:
+        if visited[nx][ny] == 0:
+          visited[nx][ny] = 1
+          queue.append((nx, ny))
 
-print(bfs())
 
-print(visited)
+N = int(input())
+citys = [list(map(int, input().split())) for _ in range(N)]
+ans = 0
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+for h in range(101):
+  visited = [[0] * N for _ in range(N)]
+  cnt = 0
+  for hi in range(N):
+    for hj in range(N):
+      if citys[hi][hj] <= h:
+        visited[hi][hj] = 1
+
+  for i in range(N):
+    for j in range(N):
+      if visited[i][j] == 0:
+        visited[i][j] = 1
+        cnt += 1
+        bfs(i, j)
+
+  ans = max(ans, cnt)
+
+print(ans)
