8a21f796c1a156c8b69669e04ce601be0a5103bd
roa_0318 <roa_0318@naver.com>
2024-06-22 15:15:17 +0900

    24.06.22

--- resolve.py
+++ resolve.py
@@ -3,52 +3,32 @@
 
 from collections import deque
 
-def bfs():
-  ans = 0
+def bfs(sx, sy):
   queue = deque()
-  
-  for k in range(H):
-    for i in range(N):
-      for j in range(M):
-        if boxs[k][i][j] == 1:
-          queue.append((k, i, j))
-          
+  queue.append((sx, sy))
+  visited[sx][sy] = 1
+
   while queue:
-    z, x, y = queue.popleft()
+    x, y = queue.popleft()
 
-    for v in range(2):
-      nz = z + dz[v]
+    if x == ex and y == ey:
+      return visited[x][y] - 1
 
-      if 0 <= nz < H:
-        if boxs[nz][x][y] == 0:
-          boxs[nz][x][y] = boxs[z][x][y] + 1
-          queue.append((nz, x, y))
+    for i, j in [(-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]:
+      nx = x + i
+      ny = y + j
 
-    for c in range(4):
-      nx = x + dx[c]
-      ny = y + dy[c]
-
-      if 0 <= nx < N and 0 <= ny < M:
-        if boxs[z][nx][ny] == 0:
-          boxs[z][nx][ny] = boxs[z][x][y] + 1
-          queue.append((z, nx, ny))
+      if 0 <= nx < I and 0 <= ny < I:
+        if not visited[nx][ny]:
+          visited[nx][ny] = visited[x][y] + 1
+          queue.append((nx, ny))
 
 
-  for h in range(H):
-    for n in range(N):
-      for m in range(M):
-        if boxs[h][n][m] == 0:
-          return -1
-        ans = max(ans, boxs[h][n][m])
+T = int(input())
+for _ in range(T):
+  I = int(input())
+  sx, sy = map(int, input().split())
+  ex, ey = map(int, input().split())
+  visited = [[0] * I for _ in range(I)]
 
-  return ans - 1
-
-
-M, N, H = map(int, input().split())
-boxs = [[list(map(int,input().split())) for _ in range(N)] for _ in range(H)]
-
-dz = [-1, 1]
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-print(bfs())
+  print(bfs(sx, sy))
