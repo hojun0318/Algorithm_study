f6227ae4c8337ba0f5c251932ef0cd40ba82a6d3
roa_0318 <roa_0318@naver.com>
2024-06-22 19:09:29 +0900

    24.06.22

--- resolve.py
+++ resolve.py
@@ -3,46 +3,48 @@
 
 from collections import deque
 
-def bfs(x, y):
+def bfs():
   queue = deque()
-  queue.append((x, y))
+
+  for l in range(L):
+    for r in range(R):
+      for c in range(C):
+        if buildings[l][r][c] == 'S':
+          visited[l][r][c] = 1
+          queue.append((l, r, c))
 
   while queue:
-    x, y = queue.popleft()
+    z, x, y = queue.popleft()
 
-    for i in range(4):
+    if buildings[z][x][y] == 'E':
+      return f'Escaped in {visited[z][x][y] - 1} minute(s).'
+
+    for i in range(6):
+      nz = z + dz[i]
       nx = x + dx[i]
       ny = y + dy[i]
 
+      if 0 <= nz < L and 0 <= nx < R and 0 <= ny < C:
+        if not visited[nz][nx][ny] and buildings[nz][nx][ny] != '#':
+          visited[nz][nx][ny] = visited[z][x][y] + 1
+          queue.append((nz, nx, ny))
 
-      if 0 <= nx < N and 0 <= ny < N:
-        if visited[nx][ny] == 0:
-          visited[nx][ny] = 1
-          queue.append((nx, ny))
+  return "Trapped!"
 
+while True:
+  L, R, C = map(int, input().split())
+  if L == 0 and R == 0 and C == 0:
+    break
 
-N = int(input())
-citys = [list(map(int, input().split())) for _ in range(N)]
-ans = 0
+  visited = [[[0] * C for _ in range(R)] for _ in range(L)]  
+  buildings = []
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+  for _ in range(L):
+    buildings.append([list(map(str, input())) for _ in range(R)])
+    temp = input()
 
-for h in range(101):
-  visited = [[0] * N for _ in range(N)]
-  cnt = 0
-  for hi in range(N):
-    for hj in range(N):
-      if citys[hi][hj] <= h:
-        visited[hi][hj] = 1
+  dz = [1, -1, 0, 0, 0, 0]
+  dx = [0, 0, -1, 1, 0, 0]
+  dy = [0, 0, 0, 0, -1, 1]
 
-  for i in range(N):
-    for j in range(N):
-      if visited[i][j] == 0:
-        visited[i][j] = 1
-        cnt += 1
-        bfs(i, j)
-
-  ans = max(ans, cnt)
-
-print(ans)
+  print(bfs())
