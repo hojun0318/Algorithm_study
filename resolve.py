888a0c6354dfcee7e25cd1ea290934010dd03ac4
roa_0318 <roa_0318@naver.com>
2024-06-24 23:18:12 +0900

    24.06.24

--- resolve.py
+++ resolve.py
@@ -1,84 +1,67 @@
 from collections import deque
 
-def check(x, y):
+def bridge(x, y, c):
+  queue = deque()
+  queue.append((x, y, c))
+
+  while queue:
+    x, y, c = queue.popleft()
+
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+
+      if 0 <= nx < N and 0 <= ny < N:
+        if maps[nx][ny] == 0 and not bridge_visited[nx][ny]:
+          bridge_visited[nx][ny] = bridge_visited[x][y] + 1
+          queue.append((nx, ny, c))
+        elif maps[nx][ny] != c and maps[nx][ny] != 0:
+          return bridge_visited[x][y]
+
+  return N
+
+def bfs(x, y):
+  global cnt
   queue = deque()
   queue.append((x, y))
-  
+
   while queue:
     x, y = queue.popleft()
 
-    for d in range(4):
-      nx = x + dx[d]
-      ny = y + dy[d]
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
 
-      if 0 <= nx < N and 0 <= ny < M:
-        if icebergs[nx][ny] != 0 and not visited[nx][ny]:
+      if 0 <= nx < N and 0 <= ny < N:
+        if maps[nx][ny] == 1 and not visited[nx][ny]:
           visited[nx][ny] = 1
+          maps[nx][ny] = cnt
           queue.append((nx, ny))
 
 
-def bfs():
-  global year
-  melt = 0
-  melts = []
-  queue = deque()
-  len_queue = deque()
-
-  for i in range(N):
-    for j in range(M):
-      if icebergs[i][j]:
-        queue.append((i, j))
-        len_queue.append((i, j))
-
-  while queue:
-    x, y = queue.popleft()
-
-    for d in range(4):
-      nx = x + dx[d]
-      ny = y + dy[d]
-
-      if 0 <= nx < N and 0 <= ny < M:
-        if icebergs[nx][ny] <= 0:
-          melt += 1
-    melts.append(melt)
-    melt = 0
-
-  for l in range(len(len_queue)):
-    r = len_queue[l][0]
-    c = len_queue[l][1]
-    if icebergs[r][c] - melts[l] < 0:
-      icebergs[r][c] = 0
-    else:
-      icebergs[r][c] -= melts[l]
-  
-  year += 1
-
-N, M = map(int, input().split())
-icebergs = [list(map(int, input().split())) for _ in range(N)]
+N = int(input())
+maps = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0] * N for _ in range(N)]
+cnt = 0
+ans = 9999999
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-year = 0
 
-bfs()
+for i in range(N):
+  for j in range(N):
+    if maps[i][j] == 1 and not visited[i][j]:
+      cnt += 1
+      visited[i][j] = 1
+      maps[i][j] = cnt
+      bfs(i, j)
 
-while True:
-  ice = 0
-  visited = [[0] * M for _ in range(N)]
-  
-  for i in range(N):
-    for j in range(M):
-      if icebergs[i][j] and not visited[i][j]:
-        ice += 1
-        visited[i][j] = 1
-        check(i, j)
+for r in range(N):
+  for c in range(N):
+    bridge_visited = [[0] * N for _ in range(N)]
+    if maps[r][c] != 0:
+      cursor = maps[r][c]
+      ans = min(ans, bridge(r, c, cursor))
 
-  if ice == 0:
-    print(0)
-    break
-  elif ice == 1:
-    bfs()
-  else:
-    print(year)
-    break
+print(ans)
