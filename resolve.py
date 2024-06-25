d3d0bd67ece0fbbd8c5f1ac9843aabb78e17fa0d
roa_0318 <roa_0318@naver.com>
2024-06-26 08:02:54 +0900

    24.06.26

--- resolve.py
+++ resolve.py
@@ -8,39 +8,31 @@ def bfs(x, y, k):
   while queue:
     x, y, k = queue.popleft()
 
-    if x == H - 1 and y == W - 1:
-      return visited[x][y][k] - 1
+    if x == (N - 1) and y == (M - 1):
+      return visited[x][y][k]
 
-    if k < K:
-      for i, j in [(-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]:
-        nx = x + i
-        ny = y + j
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
 
-        if 0 <= nx < H and 0 <= ny < W:
-          if maps[nx][ny] == 0 and not visited[nx][ny][k + 1]:
+      if 0 <= nx < N and 0 <= ny < M:
+        if k < K:
+          if maps[nx][ny] == 1 and not visited[nx][ny][k + 1]:
             visited[nx][ny][k + 1] = visited[x][y][k] + 1
             queue.append((nx, ny, k + 1))
-
-    for d in range(4):
-      nx = x + dx[d]
-      ny = y + dy[d]
-
-      if 0 <= nx < H and 0 <= ny < W:
         if maps[nx][ny] == 0 and not visited[nx][ny][k]:
           visited[nx][ny][k] = visited[x][y][k] + 1
           queue.append((nx, ny, k))
-  
-  return -1
 
-K = int(input())
-W, H = map(int, input().split())
-maps = [list(map(int, input().split())) for _ in range(H)]
-visited = [[[0] * (K + 1) for _ in range(W)] for _ in range(H)]
+
+  return - 1
+
+
+N, M, K = map(int, input().split())
+maps = [list(map(int, input())) for _ in range(N)]
+visited = [[[0] * (K + 1) for _ in range(M)] for _ in range(N)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 print(bfs(0, 0, 0))
-
-for m in visited:
-  print(m)
