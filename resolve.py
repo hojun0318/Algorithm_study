6aa1d9ef02535477433f558a2bde32cba825564a
roa_0318 <roa_0318@naver.com>
2024-06-27 07:19:24 +0900

    24.06.27

--- resolve.py
+++ resolve.py
@@ -1,32 +1,34 @@
 from collections import deque
 
-def bfs(x, y, k):
+def bfs(x, y, k, d):
   queue = deque()
-  queue.append((x, y, k))
+  queue.append((x, y, k, d))
   visited[x][y][k] = 1
 
   while queue:
-    x, y, k = queue.popleft()
+    x, y, k, d = queue.popleft()
 
     if x == (N - 1) and y == (M - 1):
       return visited[x][y][k]
-
+    
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
 
       if 0 <= nx < N and 0 <= ny < M:
-        if k < K:
-          if maps[nx][ny] == 1 and not visited[nx][ny][k + 1]:
-            visited[nx][ny][k + 1] = visited[x][y][k] + 1
-            queue.append((nx, ny, k + 1))
+        if maps[nx][ny] == 1 and k < K:
+          if not visited[nx][ny][k + 1]:
+            if d % 2 == 0:
+              visited[nx][ny][k + 1] = d + 2
+              queue.append((nx, ny, k + 1, d + 1))
+            else:
+              queue.append((x, y, k, d + 1))
+
         if maps[nx][ny] == 0 and not visited[nx][ny][k]:
           visited[nx][ny][k] = visited[x][y][k] + 1
-          queue.append((nx, ny, k))
-
+          queue.append((nx, ny, k, d + 1))
 
   return - 1
-
 
 N, M, K = map(int, input().split())
 maps = [list(map(int, input())) for _ in range(N)]
@@ -35,4 +37,7 @@ visited = [[[0] * (K + 1) for _ in range(M)] for _ in range(N)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-print(bfs(0, 0, 0))
+print(bfs(0, 0, 0, 0))
+
+for m in visited:
+  print(m)
