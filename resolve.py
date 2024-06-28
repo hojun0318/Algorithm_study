d0cbd247fdccc75f15d44ee1adeeb861fb11eff2
roa_0318 <roa_0318@naver.com>
2024-06-28 21:36:46 +0900

    24.06.28

--- resolve.py
+++ resolve.py
@@ -1,43 +1,57 @@
 from collections import deque
 
-def bfs(x, y, k, d):
+def bfs():
+  lst = []
   queue = deque()
-  queue.append((x, y, k, d))
-  visited[x][y][k] = 1
+  for p in range(1, P + 1):
+    for n in range(N):
+      for m in range(M):
+        if maps[n][m] == str(p):
+          queue.append((n, m, p, dis[p - 1]))
 
   while queue:
-    x, y, k, d = queue.popleft()
+    x, y, p, s = queue.popleft()
+    s -= 1
 
-    if x == (N - 1) and y == (M - 1):
-      return visited[x][y][k]
-    
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
 
       if 0 <= nx < N and 0 <= ny < M:
-        if maps[nx][ny] == 1 and k < K:
-          if not visited[nx][ny][k + 1]:
-            if d % 2 == 0:
-              visited[nx][ny][k + 1] = d + 2
-              queue.append((nx, ny, k + 1, d + 1))
-            else:
-              queue.append((x, y, k, d + 1))
+        if maps[nx][ny] == '.' or maps[nx][ny] == str(p):
+          maps[nx][ny] = p
 
-        if maps[nx][ny] == 0 and not visited[nx][ny][k]:
-          visited[nx][ny][k] = visited[x][y][k] + 1
-          queue.append((nx, ny, k, d + 1))
+          if s > 0:
+            queue.appendleft((nx, ny, p, s))
+          else:
+            queue.append((nx, ny, p, dis[p - 1]))
+        
+        if maps[nx][ny] == p:
+          if s > 0:
+            queue.appendleft((nx, ny, p, s))
 
-  return - 1
 
-N, M, K = map(int, input().split())
-maps = [list(map(int, input())) for _ in range(N)]
-visited = [[[0] * (K + 1) for _ in range(M)] for _ in range(N)]
+  for p in range(1, P + 1):
+    cnt = 0
+    for n in range(N):
+      for m in range(M):
+        if maps[n][m] == str(p) or maps[n][m] == p:
+          cnt += 1
+
+    lst.append(cnt)
+
+  return lst
+
+N, M, P = map(int, input().split())
+dis = list(map(int, input().split()))
+maps = [list(map(str, input())) for _ in range(N)]
+
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-print(bfs(0, 0, 0, 0))
+print(*bfs())
 
-for m in visited:
+
+for m in maps:
   print(m)
