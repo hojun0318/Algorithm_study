afe8fd95ae0fdd95e7be43d75d3d452f792d0360
roa_0318 <roa_0318@naver.com>
2024-06-29 23:38:24 +0900

    24.06.29

--- resolve.py
+++ resolve.py
@@ -1,57 +1,59 @@
 from collections import deque
 
-def bfs():
-  lst = []
-  queue = deque()
-  for p in range(1, P + 1):
-    for n in range(N):
-      for m in range(M):
-        if maps[n][m] == str(p):
-          queue.append((n, m, p, dis[p - 1]))
-
-  while queue:
-    x, y, p, s = queue.popleft()
-    s -= 1
-
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-
-      if 0 <= nx < N and 0 <= ny < M:
-        if maps[nx][ny] == '.' or maps[nx][ny] == str(p):
-          maps[nx][ny] = p
-
-          if s > 0:
-            queue.appendleft((nx, ny, p, s))
-          else:
-            queue.append((nx, ny, p, dis[p - 1]))
-        
-        if maps[nx][ny] == p:
-          if s > 0:
-            queue.appendleft((nx, ny, p, s))
-
-
-  for p in range(1, P + 1):
-    cnt = 0
-    for n in range(N):
-      for m in range(M):
-        if maps[n][m] == str(p) or maps[n][m] == p:
-          cnt += 1
-
-    lst.append(cnt)
-
-  return lst
-
 N, M, P = map(int, input().split())
-dis = list(map(int, input().split()))
+S = [0] + list(map(int,input().split()))
 maps = [list(map(str, input())) for _ in range(N)]
-
+castle = [deque() for _ in range(P + 1)]
+ans = [0] * (P + 1)
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-print(*bfs())
+for p in range(1, P + 1):
+  for i in range(N):
+    for j in range(M):
+      if maps[i][j] == str(p):
+        castle[p].append((i, j))
+        ans[p] += 1
 
+print(castle)
+print('-----------------------')
 
-for m in maps:
-  print(m)
+flag = True
+
+while flag:
+  flag = False
+  
+  for q in range(1, P + 1): # 1, 2
+    if not castle[q]:
+      continue
+  
+    queue = castle[q]
+    print('queue : ', queue)
+    print('-------')
+    for _ in range(S[q]):
+      if not q:
+        break
+
+      for _ in range(len(queue)):
+        x, y = queue.popleft()
+        print('x, y', x , y)
+
+        for i in range(4):
+          nx = x + (dx[i])
+          ny = y + (dy[i])
+
+          if 0 <= nx < N and 0 <= ny < M:
+            if maps[nx][ny] == '.':
+              maps[nx][ny] = str(q)
+              for m in maps:
+                print(m)
+              print('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ')
+              ans[q] += 1
+              queue.append((nx, ny))
+              print('que : ', queue)
+              flag = True
+
+    print('큐 : ', queue)
+
+print(*ans[1:])
