bb0cdaa40a39bebc1dc1601e645af17dabeed954
roa_0318 <roa_0318@naver.com>
2024-07-03 22:37:29 +0900

    24.07.03

--- resolve.py
+++ resolve.py
@@ -1,58 +1,74 @@
 from collections import deque
 
-def bfs(x, y):
+def bfs():
   global ans
-  now = deque()
-  now.append((x, y))
+  queue = deque()
+  visited = [[0] * (w + 2) for _ in range(h + 2)]
 
-  while now:
-    x, y = now.popleft()
+  for i in range(0, h + 2):
+    for j in range(0, w + 2):
+      if maps[i][j] == '.':
+        if i == 0 or i == (h + 1) or j == 0 or j == (w + 1):
+          visited[i][j] = 1
+          queue.append((i, j))
 
-    if rooms[x][y]:
+  while queue:
+    x, y = queue.popleft()
+
+    for d in range(4):
+      nx = x + dx[d]
+      ny = y + dy[d]
       
-      queue = rooms[x][y]
+      if 0 <= nx < (h + 2) and 0 <= ny < (w + 2):
+        if maps[nx][ny] == '*':
+          continue
+      
+        if not visited[nx][ny] and 'A' <= maps[nx][ny] <= 'Z':
+          if maps[nx][ny].lower() not in keys:
+            continue
 
-      for a, b in queue:
-        if not rights[a][b]:
-          rights[a][b] = 1
-          ans += 1
-
-          for i in range(4):
-            bx = a + dx[i]
-            by = b + dy[i]
-
-            if 0 <= bx < (N + 2) and 0 <= by < (N + 2):
-              if visited[bx][by] == 1:
-                now.append((bx, by))
-
-    for j in range(4):
-      nx = x + dx[j]
-      ny = y + dy[j]
-
-      if 0 <= nx < (N + 2) and 0 <= ny < (N + 2):
-        if rights[nx][ny] == 1 and not visited[nx][ny]:
+        if maps[nx][ny] == '.' and not visited[nx][ny]:
           visited[nx][ny] = 1
-          now.append((nx, ny))
+          queue.append((nx, ny))
+
+        elif maps[nx][ny] == '$':
+          ans += 1
+          maps[nx][ny] = '.'
+          visited = [[0] * (w + 2) for _ in range(h + 2)]
+          queue.append((nx, ny))
+
+        elif not visited[nx][ny] and 'a' <= maps[nx][ny] <= 'z':
+          if maps[nx][ny] not in keys:
+            keys.append(maps[nx][ny])
+            maps[nx][ny] = '.'
+            visited = [[0] * (w + 2) for _ in range(h + 2)]
+            queue.append((nx, ny))
+          else:
+            maps[nx][ny] = '.'
+            visited = [[0] * (w + 2) for _ in range(h + 2)]
+            queue.append((nx, ny))
+
+        elif not visited[nx][ny] and 'A' <= maps[nx][ny] <= 'Z':
+          if maps[nx][ny].lower() in keys:
+            maps[nx][ny] = '.'
+            queue.append((nx, ny))
 
   return ans
 
 
-N, M = map(int, input().split())
-rooms = [[deque() for _ in range(N + 2)] for _ in range(N + 2)]
-rights = [[0 for _ in range(N + 2)] for _ in range(N + 2)]
-visited = [[0 for _ in range(N + 2)] for _ in range(N + 2)]
+T = int(input())
+for _ in range(T):
+  h, w = map(int, input().split())
+  maps = [['.'] * (w + 2)] + [['.'] + list(map(str, input())) + ['.'] for _ in range(h)] + [['.'] * (w + 2)]
+  keys = []
+  key = input()
+  for i in range(len(key)):
+    if key != '0':
+      keys.append(key[i])
+  
+  ans = 0
 
-ans = 1
+  dx = [-1, 1, 0, 0]
+  dy = [0, 0, -1, 1]
 
-rights[1][1] = 1
-visited[1][1] = 1
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-for _ in range(M):
-  x, y, a, b = map(int, input().split())
-  rooms[x][y].append((a, b))
-
-
-print(bfs(1, 1))
+  print(bfs())
