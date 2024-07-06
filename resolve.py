fb617239c01f4241c4e63899635ab23fbf3aecf8
roa_0318 <roa_0318@naver.com>
2024-07-06 10:00:00 +0900

    24.07.06

--- resolve.py
+++ resolve.py
@@ -1,43 +1,11 @@
-from collections import deque
+N = int(input())
 
-def dfs(v):
-  dfs_visited[v] = 1
-  print(v, end = ' ')
+obN = list(bin(N)[2:][::-1])
 
-  graph[v].sort()
-  for i in graph[v]:
-    if not dfs_visited[i]:
-      dfs(i)
+t, ans = 1, 0
 
+for i in obN:
+  ans += int(i) * t
+  t *= 3
 
-def bfs(start):
-  queue = deque()
-  queue.append(start)
-  bfs_visited[start] = 1
-
-  while queue:
-    v = queue.popleft()
-    print(v, end = ' ')
-
-    graph[v].sort()
-    for i in graph[v]:
-      if not bfs_visited[i]:
-        queue.append(i)
-        bfs_visited[i] = 1
-
-
-N, M, V = map(int, input().split())
-
-graph = [[] for _ in range((N + 1))]
-
-dfs_visited = [0] * (N + 1)
-bfs_visited = [0] * (N + 1)
-
-for _ in range(M):
-  a, b = map(int, input().split())
-  graph[a].append(b)
-  graph[b].append(a)
-
-dfs(V)
-print()
-bfs(V)
+print(ans)
