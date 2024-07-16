9149e0336e01b394965dce7fdd90b61fb7e149d9
roa_0318 <roa_0318@naver.com>
2024-07-16 20:14:04 +0900

    24.07.16

--- resolve.py
+++ resolve.py
@@ -3,20 +3,27 @@ def dfs(n, lst):
     ans.append(lst)
     return
   
-  prev = 0
+  flag = 0
   for i in range(N):
-    if not visited[i] and prev != nums[i]:
-      prev = nums[i]
-      visited[i] = 1
-      dfs(n + 1, lst + [nums[i]])
-      visited[i] = 0
+    if lst:
+      if lst[-1] <= nums[i] and not visited[i] and flag != nums[i]:
+        flag = nums[i]
+        visited[i] = 1
+        dfs(n + 1, lst + [nums[i]])
+        visited[i] = 0
+    else:
+      if not visited[i] and flag != nums[i]:
+        flag = nums[i]
+        visited[i] = 1
+        dfs(n + 1, lst + [nums[i]])
+        visited[i]  = 0
 
 
 N, M = map(int, input().split())
 nums = sorted(list(map(int, input().split())))
 
-visited = [0] * N
 ans = []
+visited = [0] * N
 
 dfs(0, [])
 
