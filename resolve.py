133f4ea90c3b57000d2e752ac9dc7ad5ca8b86b2
roa_0318 <roa_0318@naver.com>
2024-07-21 16:41:27 +0900

    24.07.21

--- resolve.py
+++ resolve.py
@@ -1,41 +1,84 @@
-def dfs(n, cnt):
+from collections import deque
+
+def bfs(tlst):
+  queue = deque()
+  visited = [[0] * (M + 2) for _ in range(N + 2)]
+  cnt = 0
+
+  for i in range(TC):
+    if tlst[i] == 0:
+      continue
+
+    tx, ty = lst[i]
+    queue.append((tx, ty))
+    visited[tx][ty] = tlst[i]
+
+  while queue:
+    x, y = queue.popleft()
+
+    if visited[x][y] == flower:
+      continue
+
+    for d in range(4):
+      nx = x + dx[d]
+      ny = y + dy[d]
+
+      if maps[nx][ny] == 0 or visited[nx][ny] == flower:
+        continue
+
+      if visited[nx][ny] == 0:
+        if visited[x][y] < 0:
+          visited[nx][ny] = visited[x][y] - 1
+          queue.append((nx, ny))
+        else:
+          visited[nx][ny] = visited[x][y] + 1
+          queue.append((nx, ny))
+
+      else:
+        if visited[x][y] < 0:
+          if visited[nx][ny] + visited[x][y] - 1 == 0:
+            cnt += 1
+            visited[nx][ny] = flower
+        else:
+          if visited[nx][ny] + visited[x][y] + 1 == 0:
+            cnt += 1
+            visited[nx][ny] = flower
+
+  return cnt
+
+
+
+def dfs(n, gcnt, rcnt, tlst):
   global ans
 
-  if ans >= (cnt + (N - n) * 2):
-    return
-
-  if n == N:
-    ans = max(ans, cnt)
+  if n == TC:
+    if gcnt == G and rcnt == R:
+      ans = max(ans, bfs(tlst))
     return
   
-  if eggs[n][0] <= 0:
-    dfs(n + 1, cnt)
-  
-  else:
-    flag = False
-
-    for i in range(N):
-      if i == n or eggs[i][0] <= 0:
-        continue
-    
-      flag = True
-      eggs[n][0] -= eggs[i][1]
-      eggs[i][0] -= eggs[n][1]
-
-      dfs(n + 1, cnt + int(eggs[n][0] <= 0) + int(eggs[i][0] <= 0))
-
-      eggs[n][0] += eggs[i][1]
-      eggs[i][0] += eggs[n][1]
-
-    if flag == False:
-      dfs(n + 1, cnt)
+  dfs(n + 1, gcnt + 1, rcnt,     tlst + [1])
+  dfs(n + 1, gcnt,     rcnt + 1, tlst + [-1])
+  dfs(n + 1, gcnt,     rcnt,     tlst + [0])
 
 
 
-N = int(input())
-eggs = [list(map(int, input().split())) for _ in range(N)]
+N, M, G, R = map(int, input().split())
+maps = [[0] * (M + 2)] + [[0] + list(map(int, input().split())) + [0] for _ in range(N)] + [[0] * (M + 2)]
+
+flower = (N * M) + 1
 ans = 0
+lst = []
 
-dfs(0, 0)
+for i in range(1, N + 1):
+  for j in range(1, M + 1):
+    if maps[i][j] == 2:
+      lst.append((i, j))
+
+TC = len(lst)
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+dfs(0, 0, 0, [])
 
 print(ans)
