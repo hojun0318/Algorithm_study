2d2f90fbaa527a08ecf6726ecb020ae5e69280a2
roa_0318 <roa_0318@naver.com>
2024-07-21 18:18:51 +0900

    24.07.21

--- resolve.py
+++ resolve.py
@@ -1,35 +1,40 @@
 def dfs(n, cnt):
   global ans
 
-  if ans >= (cnt + L - n):
+  if ans >= (cnt + (L + 1 - n) // 2):
     return
-  
-  if n == L:
+
+  if n >= L:
     ans = max(ans, cnt)
     return
   
-  for x, y in lst[n]:
-    if visited[x - y] == 0:
-      visited[x - y] = 1
-      dfs(n + 1, cnt + 1)
-      visited[x - y] = 0
-  
-  dfs(n + 1, cnt)
+  for ci, cj in lst[n]:
+    if not visited[ci - cj]:
+      visited[ci - cj] = 1
+      dfs(n + 2, cnt + 1)
+      visited[ci - cj] = 0
+
+  dfs(n + 2, cnt)
+
 
 N = int(input())
-maps = [list(map(int, input().split())) for _ in range(N)]
-ans = 0
+arr= [list(map(int, input().split())) for _ in range(N)]
 
-lst = [[] for _ in range(2 * N)]
-
+lst = [[] for _ in range(2 * N - 1)]
 for i in range(N):
   for j in range(N):
-    if maps[i][j] == 1:
+    if arr[i][j] == 1:
       lst[i + j].append((i, j))
 
 L = 2 * N - 1
-visited = [0] * (2 * N)
+visited = [0] * (2 * N - 1)
 
+ans = 0
 dfs(0, 0)
 
-print(ans)
+tmp = ans
+
+ans = 0
+dfs(1, 0)
+
+print(ans + tmp)
