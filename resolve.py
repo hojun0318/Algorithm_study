d1791c5c1423a385ac57f406bf9dfcbff6404b39
roa_0318 <roa_0318@naver.com>
2024-07-21 16:26:00 +0900

    24.07.21

--- resolve.py
+++ resolve.py
@@ -1,62 +1,41 @@
-from collections import deque
-
-def bfs(x, y):
-  queue = deque()
-  bfs_visited = [[0] * 5 for _ in range(5)]
-  queue.append((x, y))
-  bfs_visited[x][y] = 1
-  cnt = 1
-
-  while queue:
-    x, y = queue.popleft()
-
-    for d in range(4):
-      nx = x + dx[d]
-      ny = y + dy[d]
-
-      if 0 <= nx < 5 and 0 <= ny < 5:
-        if not bfs_visited[nx][ny] and visited[nx][ny] == 1:
-          bfs_visited[nx][ny] = 1
-          cnt += 1
-          queue.append((nx, ny))
-
-  if cnt == 7:
-    return True
-
-
-def check():
-  for i in range(5):
-    for j in range(5):
-      if visited[i][j] == 1:
-        return bfs(i, j)
-
-
-def dfs(n, cnt, scnt):
+def dfs(n, cnt):
   global ans
-  
-  if cnt > 7:
-    return
-  
-  if n == 25:
-    if cnt == 7 and scnt >= 4:
-      if check():
-        ans += 1
+
+  if ans >= (cnt + (N - n) * 2):
     return
 
-  visited[n // 5][n % 5] = 1
-  dfs(n + 1, cnt + 1, scnt + int(maps[n // 5][n % 5] == 'S'))
-  visited[n // 5][n % 5] = 0
+  if n == N:
+    ans = max(ans, cnt)
+    return
+  
+  if eggs[n][0] <= 0:
+    dfs(n + 1, cnt)
+  
+  else:
+    flag = False
 
-  dfs(n + 1, cnt, scnt)
+    for i in range(N):
+      if i == n or eggs[i][0] <= 0:
+        continue
+    
+      flag = True
+      eggs[n][0] -= eggs[i][1]
+      eggs[i][0] -= eggs[n][1]
+
+      dfs(n + 1, cnt + int(eggs[n][0] <= 0) + int(eggs[i][0] <= 0))
+
+      eggs[n][0] += eggs[i][1]
+      eggs[i][0] += eggs[n][1]
+
+    if flag == False:
+      dfs(n + 1, cnt)
 
 
-maps = [list(map(str, input())) for _ in range(5)]
-visited = [[0] * 5 for _ in range(5)]
+
+N = int(input())
+eggs = [list(map(int, input().split())) for _ in range(N)]
 ans = 0
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-dfs(0, 0, 0)
+dfs(0, 0)
 
 print(ans)
