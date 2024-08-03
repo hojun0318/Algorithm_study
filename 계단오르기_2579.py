8515fc3d57b200ff5087282152f8d5a18a939c80
roa_0318 <roa_0318@naver.com>
2024-08-04 02:41:07 +0900

    24.08.04

---
+++ 계단오르기_2579.py
@@ -0,0 +1,11 @@
+N = int(input())
+stair = [0] + [int(input()) for _ in range(N)]
+
+dp = [[0] * 3 for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+  dp[i][0] = max(dp[i - 1][1], dp[i - 1][2])
+  dp[i][1] = dp[i - 1][0] + stair[i]
+  dp[i][2] = dp[i - 1][1] + stair[i]
+
+print(max(dp[N][1:3]))
