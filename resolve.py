fb5bced3a4d48be8769639f2cd95cd25f4f8a3f2
roa_0318 <roa_0318@naver.com>
2024-08-04 03:22:23 +0900

    24.08.04

--- resolve.py
+++ resolve.py
@@ -1,40 +1,13 @@
-def dfs(n, cnt):
-  global ans
-
-  if ans >= (cnt + (L + 1 - n) // 2):
-    return
-
-  if n >= L:
-    ans = max(ans, cnt)
-    return
-  
-  for ci, cj in lst[n]:
-    if not visited[ci - cj]:
-      visited[ci - cj] = 1
-      dfs(n + 2, cnt + 1)
-      visited[ci - cj] = 0
-
-  dfs(n + 2, cnt)
-
-
 N = int(input())
-arr= [list(map(int, input().split())) for _ in range(N)]
 
-lst = [[] for _ in range(2 * N - 1)]
-for i in range(N):
-  for j in range(N):
-    if arr[i][j] == 1:
-      lst[i + j].append((i, j))
+dp = [0] * (N + 1)
 
-L = 2 * N - 1
-visited = [0] * (2 * N - 1)
+for i in range(2, N + 1):
+  dp[i] = dp[i - 1] + 1
+  if i % 2 == 0:
+    dp[i] = min(dp[i], dp[i // 2] + 1)
+  if i % 3 == 0:
+    dp[i] = min(dp[i], dp[i // 3] + 1)
+    
 
-ans = 0
-dfs(0, 0)
-
-tmp = ans
-
-ans = 0
-dfs(1, 0)
-
-print(ans + tmp)
+print(dp[N])
