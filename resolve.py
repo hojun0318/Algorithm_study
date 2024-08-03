771d42d7f20516f7d64929f358703b0a436fa310
roa_0318 <roa_0318@naver.com>
2024-08-04 03:34:52 +0900

    24.08.04

--- resolve.py
+++ resolve.py
@@ -1,11 +1,11 @@
-T = int(input())
-dp = [0] * 12
-dp[1:4] = [1, 2, 4]
+N = int(input())
+stair = [0] + list(int(input()) for _ in range(N))
 
-for _ in range(T):
-  N = int(input())
+dp = [[0] * 3 for _ in range(N + 1)]
 
-  for i in range(4, 12):
-    dp[i] = sum(dp[i - 3: i])
+for i in range(1, N + 1):
+  dp[i][0] = max(dp[i - 1][1], dp[i - 1][2])
+  dp[i][1] = dp[i - 1][0] + stair[i]
+  dp[i][2] = dp[i - 1][1] + stair[i]
 
-  print(dp[N])
+print(max(dp[N][1:3]))
