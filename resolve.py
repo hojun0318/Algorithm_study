ea058b61979f477203fbca5698d1cab7bbb95c41
roa_0318 <roa_0318@naver.com>
2024-08-06 21:14:09 +0900

    24.08.06

--- resolve.py
+++ resolve.py
@@ -1,71 +1,24 @@
-H, W = map(int, input().split())
-N = int(input())
-stickers = []
+def dfs(n, tot, cnt):
+  global ans
+
+  # 3번, 종료조건 설정
+  if n == N:
+    # 조건1 : 해당 부분수열의 합이 S인가, 조건2: 크기가 양수인가(최소 1개 이상)
+    if tot == S and cnt > 0:
+      ans += 1
+    return
+  
+  # 2진 트리(2^20 약 10^6)
+  # 5번 해당 숫자를 포함하는 경우 / 포함하지 않는 경우
+  dfs(n + 1, tot + lst[n], cnt + 1)
+  dfs(n + 1, tot, cnt)
+
+# 1번, 입력 받기
+N, S = map(int, input().split())
+lst = list(map(int, input().split()))
 ans = 0
 
-for _ in range(N):
-  R, C = map(int, input().split())
-  area = R * C
-  # 넓이, 로우, 컬럼
-  stickers.append([area, R, C])
-
-for i in range(N - 1):
-  cnt = flag = 0
-  size1, x1, y1 = stickers[i]
-
-  # 가지치기
-  if max(x1, y1) > max(H,W):
-    continue
-
-  flag += 1
-  cnt += size1
-
-  for j in range(i + 1, N):
-    size2, x2, y2 = stickers[j]
-    # 첫번째 스티커 회전 X
-    if x1 <= H and y1 <= W:
-      # 두번째 스티커 회전 X
-      if x2 <= (H - x1) and y2 <= W:
-        cnt += size2
-        flag += 1
-      elif x2 <= H and y2 <= (W - y1):
-        cnt += size2
-        flag += 1
-      # 두번째 스티커 회전 O
-      elif y2 <= (H - x1) and x2 <= W:
-        cnt += size2
-        flag += 1
-      elif y2 <= H and x2 <= (W - y1):
-        cnt += size2
-        flag += 1
-    # 스티커 2개 여부 확인
-    if flag == 2:
-      if cnt > ans:
-        ans = cnt
-      flag -= 1
-      cnt -= size2
-    # 첫번째 스티커 회전 O
-    if y1 <= H and x1 <= W:
-      # 두번째 스티커 회전 X
-      if x2 <= (H - y1) and y2 <= W:
-        cnt += size2
-        flag += 1
-      elif x2 <= H and y2 <= (W - x1):
-        cnt += size2
-        flag += 1
-      # 두번째 스티커 회전 O
-      elif y2 <= (H - y1) and x2 <= W:
-        cnt += size2
-        flag += 1
-      elif y2 <= H and x2 <= (W - x1):
-        cnt += size2
-        flag += 1
-    # 스티커 2개 여부 확인
-    if flag == 2:
-      if cnt > ans:
-        ans = cnt
-      flag -= 1
-      cnt -= size2
-
+# 2번, n을 인덱스로 설정하고, N - 1까지 즉, 이진트리 말단 노드까지 탐색
+dfs(0, 0, 0)
 
 print(ans)
