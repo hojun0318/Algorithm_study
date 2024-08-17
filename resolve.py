2860f08def360550434678f7f39e4e11de690e96
roa_0318 <roa_0318@naver.com>
2024-08-17 22:53:58 +0900

    24.08.17

--- resolve.py
+++ resolve.py
@@ -1,24 +1,17 @@
-def dfs(n, tot, cnt):
-  global ans
+# 입력 받기
+N = int(input())
+stair = [0] + [int(input()) for _ in range(N)]
 
-  # 3번, 종료조건 설정
-  if n == N:
-    # 조건1 : 해당 부분수열의 합이 S인가, 조건2: 크기가 양수인가(최소 1개 이상)
-    if tot == S and cnt > 0:
-      ans += 1
-    return
-  
-  # 2진 트리(2^20 약 10^6)
-  # 5번 해당 숫자를 포함하는 경우 / 포함하지 않는 경우
-  dfs(n + 1, tot + lst[n], cnt + 1)
-  dfs(n + 1, tot, cnt)
+# dp 테이블 생성 및 초기화
+dp = [[0] * 3 for _ in range(N + 1)]
 
-# 1번, 입력 받기
-N, S = map(int, input().split())
-lst = list(map(int, input().split()))
-ans = 0
+for i in range(1, N + 1):
+  # 안밟는 경우
+  dp[i][0] = max(dp[i - 1][1], dp[i - 1][2])
+  # 1번 밟는 경우
+  dp[i][1] = dp[i - 1][0] + stair[i]
+  # 2번 연속으로 밟는 경우
+  dp[i][2] = dp[i - 1][1] + stair[i]
 
-# 2번, n을 인덱스로 설정하고, N - 1까지 즉, 이진트리 말단 노드까지 탐색
-dfs(0, 0, 0)
-
-print(ans)
+# 마지막 계단은 무조건 밟아야 함
+print(max(dp[N][1:3]))
