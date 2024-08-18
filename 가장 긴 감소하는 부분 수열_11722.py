d473556679b77d3a34e9cdc56bb305e53075c867
roa_0318 <roa_0318@naver.com>
2024-08-18 20:48:05 +0900

    24.08.18

--- 가장 긴 감소하는 부분 수열_11722.py
+++ 가장 긴 감소하는 부분 수열_11722.py
@@ -1,15 +1,16 @@
+# 입력 받기
 N = int(input())
 lst = [1000] + list(map(int, input().split()))
-
+# dp 테이블 생성 및 초기화
 dp = [0] * (N + 1)
 
 for i in range(1, N + 1):
   mx = 0
+  # 0 ~ i - 1 중 max값 (현재보다 큰 값 중에서)
   for j in range(0, i):
     if lst[i] < lst[j]:
       mx = max(mx, dp[j])
   dp[i] = mx + 1
 
 
-print(dp)
 print(max(dp))
