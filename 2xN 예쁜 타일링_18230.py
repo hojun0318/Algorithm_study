1fe03d47330dcdd9cf248f63024b655a632da9ce
roa_0318 <roa_0318@naver.com>
2024-08-23 23:09:38 +0900

    24.08.23

---
+++ 2xN 예쁜 타일링_18230.py
@@ -0,0 +1,25 @@
+N, A, B = map(int, input().split()) 
+lst2_1 = sorted(list(map(int, input().split()))) 
+lst2_2 = sorted(list(map(int, input().split())))    
+ans = 0 
+
+if N % 2 == 1: 
+  ans += lst2_1[-1] 
+  lst2_1.pop(-1) 
+  N -= 1 
+
+for _ in range(0, N, 2): 
+  t1, t2 = 0, 0 
+  if len(lst2_1) >= 2: 
+    t1 = lst2_1[-1] + lst2_1[-2] 
+  if len(lst2_2) >= 1: 
+    t2 = lst2_2[-1] 
+        
+  if t1 > t2: 
+    ans += t1 
+    lst2_1.pop(); lst2_1.pop() 
+  else: 
+    ans += t2 
+    lst2_2.pop() 
+
+print(ans)
