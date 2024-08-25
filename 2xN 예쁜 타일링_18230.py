5b582cbce0c6ca5301fa4fd6fa89a3f11b5a460b
roa_0318 <roa_0318@naver.com>
2024-08-25 19:46:30 +0900

    24.08.25

--- 2xN 예쁜 타일링_18230.py
+++ 2xN 예쁜 타일링_18230.py
@@ -1,26 +1,28 @@
-N, A, B = map(int, input().split()) 
-lst2_1 = sorted(list(map(int, input().split()))) 
-lst2_2 = sorted(list(map(int, input().split())))    
-ans = 0 
+N, A, B = map(int, input().split())
+a_2x1 = sorted(list(map(int, input().split())))
+b_2x2 = sorted(list(map(int, input().split())))
 
-if N % 2 == 1: 
-  ans += lst2_1[-1] 
-  lst2_1.pop(-1) 
-  N -= 1 
+ans = 0
 
-for _ in range(0, N, 2): 
-  t1, t2 = 0, 0 
-  if len(lst2_1) >= 2: 
-    t1 = lst2_1[-1] + lst2_1[-2] 
-  if len(lst2_2) >= 1: 
-    t2 = lst2_2[-1] 
-        
-  if t1 > t2: 
-    ans += t1 
-    lst2_1.pop()
-    lst2_1.pop() 
-  else: 
-    ans += t2 
-    lst2_2.pop() 
+if N % 2 == 1:
+  ans += a_2x1[-1]
+  a_2x1.pop()
+  N -= 1
+
+for _ in range(0, N, 2):
+  tile_A, tile_B = 0, 0
+  if len(a_2x1) >= 2:
+    tile_A = a_2x1[-1] + a_2x1[-2]
+  if len(b_2x2) >= 1:
+    tile_B = b_2x2[-1]
+
+  if tile_A > tile_B:
+    ans += tile_A    
+    for _ in range(2):
+      a_2x1.pop()
+  else:
+    ans += tile_B
+    b_2x2.pop()
+
 
 print(ans)
--- resolve.py
+++ resolve.py
@@ -1,17 +1,16 @@
-# 입력 받기
-N = int(input())
-stair = [0] + [int(input()) for _ in range(N)]
+N, K = map(int, input().split())
+height = sorted(list(map(int, input().split())))
 
-# dp 테이블 생성 및 초기화
-dp = [[0] * 3 for _ in range(N + 1)]
+lst = []
 
-for i in range(1, N + 1):
-  # 안밟는 경우
-  dp[i][0] = max(dp[i - 1][1], dp[i - 1][2])
-  # 1번 밟는 경우
-  dp[i][1] = dp[i - 1][0] + stair[i]
-  # 2번 연속으로 밟는 경우
-  dp[i][2] = dp[i - 1][1] + stair[i]
+for i in range(N - 1):
+  n = height[i + 1] - height[i]
+  lst.append(n)
 
-# 마지막 계단은 무조건 밟아야 함
-print(max(dp[N][1:3]))
+lst.sort()
+cost = 0
+
+for i in range(N - K):
+  cost += lst[i]
+
+print(cost)
