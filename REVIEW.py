4305cc80353f0eae51b36352c00d49ed6eb540ee
roa_0318 <roa_0318@naver.com>
2024-10-13 21:58:29 +0900

    24.10.13

--- REVIEW.py
+++ REVIEW.py
@@ -1,11 +1,12 @@
-T = int(input())
+N = int(input())
+costs = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * 3 for _ in range(N)]
 
-dp = [0] * 12
-dp[1:4] = [1, 2, 4]
+dp[0] = costs[0]
 
-for i in range(4, 12):
-  dp[i] = sum(dp[i - 3 : i])
+for i in range(1, N):
+  dp[i][0] = costs[i][0] + min(dp[i - 1][1], dp[i - 1][2])
+  dp[i][1] = costs[i][1] + min(dp[i - 1][0], dp[i - 1][2])
+  dp[i][2] = costs[i][2] + min(dp[i - 1][0], dp[i - 1][1])
 
-for _ in range(T):
-  n = int(input())
-  print(dp[n])
+print(min(dp[N - 1]))
